a1666bc3a768604f0c7fe35b54a2548814c1a21e
Rebekah <rbk1008@Gmail.com>
2021-11-16 23:12:37 -0700

    commit tree 352047e70d29c3842084b6b4a40f130e68badee6

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ function fetchBooks() {
   
 }
 
+fetch("https://anapioficeandfire.com/api/books")
+.then ((res) => res.json())
+.then(renderBooks)
+
+
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
     h2.innerHTML = book.name;
     main.appendChild(h2);
+    return (fetch);
   });
 }
 
